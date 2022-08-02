3dff3cf687348becee37e2f53f10857da63a77aa
OmarMWarraich <earthling986@gmail.com>
2022-08-02 21:41:31 +0500

    feat: add ham menu items

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const toggle = document.querySelector('.nav_icon'); 
+const menu = document.querySelector('.menu');
+const main = document.querySelector('.main-content');
+let i = 0;
 
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('open');
+    main.style.display = 'none';
+    menu.style.display = 'block';
+    i += 1;
+    if (i % 2 === 0) {
+        menu.style.display = 'none';
+        main.style.display = 'flex';
+    }
 });
+
+
+
+
 
