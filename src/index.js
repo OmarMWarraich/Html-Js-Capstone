5ee660086e02241b0a740973e37f6798edf21f1a
OmarMWarraich <earthling986@gmail.com>
2022-08-03 01:45:25 +0500

    feat: add program section

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-const toggle = document.querySelector('.nav_icon'); 
+const toggle = document.querySelector('.toggle_icon'); 
 const menu = document.querySelector('.menu');
 const main = document.querySelector('.main-content');
+const program = document.querySelector('.program');
+
 let i = 0;
 
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('open');
     main.style.display = 'none';
     menu.style.display = 'block';
+    program.style.display = 'none';
     i += 1;
     if (i % 2 === 0) {
         menu.style.display = 'none';
         main.style.display = 'flex';
+        program.style.display = 'flex';
     }
 });
+
+
 
 
 
